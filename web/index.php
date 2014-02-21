f0ad72a19297e2b155b9b865784d109ba2a0bc81
alec <alec.hulce@gmail.com>
2014-02-21 14:08:05 -0800

    sup

---
+++ web/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Hope Chapel Long Beach - Site Under Construction</title>
+<style type="text/css">
+body
+{
+font-family: arial,sans-serif;
+font-size: 12px;
+}
+
+body,h1,h2,ul,li,p
+{
+margin: 0;
+padding: 0;
+}
+
+ul
+{
+list-style: none;
+}
+
+h1
+{
+font-size: 18px;
+padding-bottom: 8px;
+}
+
+h2
+{
+font-size: 16px;
+padding-bottom: 6px;
+}
+
+#cont
+{
+width: 700px;
+margin: 0 auto;
+padding: 20px 0 20px 0;
+}
+
+.header
+{
+text-align: center;
+padding-bottom: 20px;
+}
+
+.suc
+{
+text-align: center;
+padding-bottom: 30px;
+}
+
+.suc h1
+{
+text-align: center;
+}
+
+.suc p
+{
+font-size: 14px;
+}
+
+.services
+{
+padding-bottom: 30px;
+}
+
+.services ul li
+{
+padding-left: 10px;
+padding-bottom: 1px;
+}
+
+.services ul li strong
+{
+margin-right: 0.6em;
+}
+
+.services ul li span
+{
+margin-left: 0.4em;
+}
+
+.addr address
+{
+font-style: normal;
+padding-bottom: 12px;
+}
+
+.addr a
+{
+color: #0000ff;
+}
+</style>
+</head>
+<body>
+
+<div id="cont">
+	<div class="header"><img src="http://hopechapellongbeach.com/images/hc_logo.gif" alt="Hope Chapel Long Beach" /></div>
+	<div class="suc">
+		<h1>Site Under Construction</h1>
+		<p>We are currently working on a fresh design! Visit us later to check it out.</p>
+	</div>
+	<div class="services">
+		<h2>Services</h2>
+		<ul>
+			<li><strong>Main Service</strong>-<span>Sunday 10 am to 11:30ish, @Church, Worship and Sermon, Sunday school and young child care available</span></li>
+			<li><strong>High School Group</strong>-<span>Tuesday 7:30-9pm, @Church</span></li>
+			<li><strong>Junior High Group</strong>-<span>Tuesday 7:30-9pm, @Church</span></li>
+			<!--<li><strong>Men and Women's Life Transformation Groups (separate)</strong>-<span>Tuesday 7pm, @McDonald's on Carson/Los Coyotes near LB Town Center</span></li>-->
+			<li><strong>Women's Prayer Group</strong>-<span>Tuesday 7:30pm, @Upton Home (call for directions)</span></li>
+			<li><strong>College/Career Group</strong>-<span>check with Lauren Schoepfer</span></li>
+			<li><strong>Men's Fellowship</strong>-<span>Wednesday 7:30pm, @Church, 1st 3rd and 5th Wednesdays of each month only</span></li>
+			<li><strong>Women's Study</strong>-<span>Wednesday (on break)</span></li>
+			<li><strong>Midweek Bible Study</strong>-<span>Thursday 7:30pm, @Church</span></li>
+			<!--<li><strong>High School Home Study</strong>-<span>Friday 6pm, @Pocoroba home</span></li>-->
+			<li><strong>Prayer Meeting</strong>-<span>Friday 7:30-9:30pm, @Church</span></li>
+			<li><strong>Door-to-Door Evangelism</strong>-<span>Saturday 9:30am, meet @Church</span></li>
+		</ul>
+	</div>
+	<div class="addr">
+		<h2>Address</h2>
+		<address>
+			Hope Chapel Long Beach
+			<br />(562) 597-1696
+			<br /><a href="mailto:friends@hopechapellongbeach.org">friends@hopechapellongbeach.org</a>
+			<br /><a href="mailto:ahulce@gmail.com">ahulce@gmail.com</a> (for web-related inquiries)
+			<br />5555 E Stearns St, Suite 210
+			<br />Long Beach, CA 90815
+		</address>
+		<a href="http://maps.google.com/maps?f=d&source=s_d&saddr=&daddr=5555+Stearns+Street,+Long+Beach,+CA&hl=en&geocode=FeSvAwIdFZT1-Clr231n7zHdgDElGu2d-Xmj_g&mra=ls&sll=35.81177,-120.26546&sspn=5.309218,9.645996&ie=UTF8&z=7" target="_blank">Get directions from Google Maps</a>
+		<br /><a href="http://www.mapquest.com/directions?form=directions&2pn=Hope%20Chapel%20Long%20Beach&2a=5555%20E%20Stearns%20St&2c=Long%20Beach&2s=CA&2z=90815&2y=US&2l=33.796073&2g=-118.123504" target="_blank">Get Directions from Mapquest</a>
+	</div>
+</div>
+
+</body>
+</html>
