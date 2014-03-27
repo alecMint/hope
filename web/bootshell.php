0122aeb7659015b0b2a8ff148076d58941fe493b
alec <alec.hulce@gmail.com>
2014-03-27 15:18:33 -0700

    u

--- web/bootshell.php
+++ web/bootshell.php
@@ -5,7 +5,9 @@ define('WEBROOT',dirname(__DIR__));
 include WEBROOT.'/helpers/common.php';
 include WEBROOT.'/helpers/Ace.php';
 
-\Ace::loadConfig( WEBROOT.'/config.php', WEBROOT.'/config.override.php' );
+var_dump(class_exists('Ace'));exit;
+
+Ace::loadConfig( WEBROOT.'/config.php', WEBROOT.'/config.override.php' );
 
 echo WEBROOT.'4444';
 exit;
