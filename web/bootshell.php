41511903c524f5addf66c9f7760843cf7a75c796
alec <alec.hulce@gmail.com>
2014-03-27 15:13:21 -0700

    u

--- web/bootshell.php
+++ web/bootshell.php
@@ -2,10 +2,10 @@
 
 define('WEBROOT',dirname(__DIR__));
 
-echo WEBROOT.'wefwef';
-exit;
-
 include WEBROOT.'/helpers/common.php';
+
+echo WEBROOT.'wefwefefwefwefewf';
+exit;
 include WEBROOT.'/helpers/Ace.php';
 
 Ace::loadConfig( WEBROOT.'/config.php', WEBROOT.'/config.override.php' );
