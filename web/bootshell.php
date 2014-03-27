81477236c606cb5845c2365057a8de5b5d8214eb
alec <alec.hulce@gmail.com>
2014-03-27 15:26:54 -0700

    u

--- web/bootshell.php
+++ web/bootshell.php
@@ -5,11 +5,12 @@ ini_set('display_errors',1);error_reporting(E_ALL);
 define('WEBROOT',dirname(__DIR__));
 define('APP_PATH',WEBROOT.'/ace');
 
+var_dump(WEBROOT);
+var_dump(APP_PATH);
+
 include APP_PATH.'/helpers/common.php';
 include APP_PATH.'/Ace.php';
 
-var_dump(WEBROOT);
-var_dump(APP_PATH);
 var_dump(class_exists('Ace'));
 
 \ace\Ace::loadConfig( WEBROOT.'/config.php', WEBROOT.'/config.override.php' );
