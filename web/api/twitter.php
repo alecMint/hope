36ef8ad8d82ec8264a7bcd4fb0ab0741b3009301
alec <alec.hulce@gmail.com>
2014-03-27 10:15:24 -0700

    u

--- web/api/twitter.php
+++ web/api/twitter.php
@@ -5,21 +5,33 @@ $key = 'HFUeBKXV6OLp1PYXl3RVzQ';
 $secret = 'kMBnmyGwgmcpWRNyXPPGDmMhRanKHkVOxDSkUU4M6Kc';
 $creds =  base64_encode(rawurlencode($key).':'.rawurlencode($secret));
 
+
+/*
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_USERPWD, $creds);
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
-//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token');
-#curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token?screen_name=twitterapi&count=2');
+//curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token?screen_name=twitterapi&count=2');
+*/
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Authorization: $creds"
+));
+
 $r = curl_exec($ch);
 
 echo 'sup<br />';
-echo "$creds<br />";
+//echo "$creds<br />";
 echo $r;
 
 
 /*
-  
+SEZVZUJLWFY2T0xwMVBZWGwzUlZ6UTprTUJubXlHd2dtY3BXUk55WFBQR0RtTWhSYW5LSGtWT3hEU2tVVTRNNktj
+
 */
