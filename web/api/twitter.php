c003ce5ffcee563db34941bdbb35481accb9dbcc
alec <alec.hulce@gmail.com>
2014-03-27 10:08:19 -0700

    u

--- web/api/twitter.php
+++ web/api/twitter.php
@@ -7,12 +7,12 @@ $creds =  base64_encode(rawurlencode($key).':'.rawurlencode($secret));
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_USERPWD, $creds);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_POST, 1);
-//curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token');
-curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token?screen_name=twitterapi&count=2');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token');
+#curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/oauth2/token?screen_name=twitterapi&count=2');
 $r = curl_exec($ch);
 
 echo 'sup<br />';
