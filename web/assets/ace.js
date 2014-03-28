8f3079c00b3ecbcf390e4b13d0e5ecb1c3b65073
alec <alec.hulce@gmail.com>
2014-03-27 18:15:21 -0700

    u

--- web/assets/ace.js
+++ web/assets/ace.js
@@ -814,7 +814,7 @@ ace.ui.register('twitter',{
     ;
     if (z.opts.scroll)
       z.$.cont.addClass('is-scroll-'+z.opts.scroll);
-    z.$.cont.html('<div class="'+x+'"-tweets_cont"></div>');
+    z.$.cont.html('<div class="'+x+'-tweets_cont"></div>');
     z.$.tweetsCont = z.$.cont.find('div.'+x+'-tweets_cont');
     z.$.tweets = $([]);
     $.each(z.data,function(i,tweet){
