c1d77ad2d3938290476b90745249874852f755b5
alec <alec.hulce@gmail.com>
2014-04-04 20:00:54 -0700

    u

--- web/wp-content/themes/ecclesia/template-parts/mod-footer-scripts.php
+++ web/wp-content/themes/ecclesia/template-parts/mod-footer-scripts.php
@@ -11,7 +11,7 @@ jQuery(document).ready(function(){
     jQuery('#main-nav').pragmaNavigation({
         subnavsOffsetLeft: 0
         ,subnavsOffsetTop: -15
-        ,eventTimeoutTime: 1000
+        ,eventTimeoutTime: 700
     });
     
     /* Front page slider */
