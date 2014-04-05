255c4339dca67546673be1b9d937134d378c0e1e
alec <alec.hulce@gmail.com>
2014-04-04 21:56:31 -0700

    u

--- web/assets/jquery.plugins.js
+++ web/assets/jquery.plugins.js
@@ -294,8 +294,8 @@ jQuery.fn.pragmaNavigation = function(conf) {
         if ( (itemPosLeft + subnavWidth) > (contentBoundryRight()) ) {
           itemPosLeft = itemPos.left - subnavWidth - conf.subnavsOffsetLeft;
         }
+        console.log('pstop',item.offset(),subnav.offset(),subnav.height());
       }
-      console.log('pstop',item.offset(),subnav.offset(),subnav.height());
       subnav.css({ top: itemPosTop, left: itemPosLeft });
     }
     
