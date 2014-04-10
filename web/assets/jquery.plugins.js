c6fe3d81d82a358dbad63307832e4c3fe723d541
alec <alec.hulce@gmail.com>
2014-04-09 18:59:42 -0700

    u

--- web/assets/jquery.plugins.js
+++ web/assets/jquery.plugins.js
@@ -179,7 +179,7 @@ jQuery.fn.pragmaNavigation = function(conf) {
     }
 
     function itemAddMouseEvents(item,subnav) {
-      item.mouseenter(function(){
+      item.mouseover(function(){
         var isTopItem = item.hasClass('top-menu-item')
         if (isTopItem)
           topItems.removeClass('item-hover')
@@ -205,7 +205,7 @@ jQuery.fn.pragmaNavigation = function(conf) {
           subnav.animate({height: "show"}, conf.slideDownSpeed, conf.slideDownEffect);
           positionSubnav(item,subnav);
         }
-      }).mouseleave(function(){
+      }).mouseout(function(){
         var itemTimeout = setItemTimeout(item);
         var subnavTimeout = setSubnavTimeout(subnav);
         timeouts[ jqd(item,'UID') ] = itemTimeout;
