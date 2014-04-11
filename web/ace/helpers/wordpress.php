fb702060b4d7e1c4dec54fcc485d53e057865f06
alec <alec.hulce@gmail.com>
2014-04-10 18:15:26 -0700

    u

--- web/ace/helpers/wordpress.php
+++ web/ace/helpers/wordpress.php
@@ -5,5 +5,5 @@ remove_filter( 'the_excerpt', 'wpautop' );
 add_action('wp_head', 'wordpress_hook_wphead');
 
 function wordpress_hook_wphead(){
-  echo '<meta name="description" content="Hope Chapel Long Beach, 5555 Stearns. Sunday service begins at 10:00am." />'
+  echo '<meta name="description" content="Hope Chapel Long Beach, 5555 Stearns. Sunday service begins at 10:00am." />';
 }
