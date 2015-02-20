b936450885658e54d41bc95cf2653679344ec12e
alec <alec.hulce@gmail.com>
2015-02-20 02:13:52 -0800

    ಠ_ಠ

--- web/wp-login.php
+++ web/wp-login.php
@@ -13,6 +13,7 @@ require( dirname(__FILE__) . '/wp-load.php' );
 
 use \ace\helpers\Protect;
 var_dump('her');
+var_dump(class_exists('Protect'));
 //Protect::preventBruteForce(1, true); // 1 call per second
 
 // Redirect to https login if forced to use SSL
